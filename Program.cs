fcc124b3c9cc1de3275e7eb88fa5af25bdff1062
olgaaktas <binpolyglotte@gmail.com>
2025-01-06 10:25:35 +0100

    add post game endpoint and dto

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using GameStore.Dto;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndPointName = "GetGame";
+
 List<GameDto> games = [
     new (
     1, 
@@ -22,6 +24,20 @@ List<GameDto> games = [
 
 app.MapGet("games", () => games);
 
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
+.WithName(GetGameEndPointName);
+
+app.MapPost("games", (CreateGameDto game) => {
+    GameDto newGame = new(
+        games.Count+1, 
+        game.Name, 
+        game.Genre, 
+        game.Price, 
+        game.ReleaseDate
+        );
+    games.Add(newGame);
+
+    return Results.CreatedAtRoute(GetGameEndPointName, new{id = newGame.Id}, game);
+    });
 
 app.Run();
