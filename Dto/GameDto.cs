323a6124fa067c2691bfbf7a43176617de1e772d
olgaaktas <binpolyglotte@gmail.com>
2025-01-06 09:48:30 +0100

    add gamedto and games endpoint

---
+++ Dto/GameDto.cs
@@ -0,0 +1,8 @@
+namespace GameStore.Dto;
+
+public record class GameDto(
+    int Id, 
+    string Name, 
+    string Genre, 
+    decimal Price, 
+    DateOnly ReleaseDate);
--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
+using GameStore.Dto;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Olga!");
+List<GameDto> games = [
+    new (
+    1, 
+    "War Craft", 
+    "Strategy", 
+    12.3M, 
+    new DateOnly(1999, 1, 1)
+    ),
+    new (
+    2, 
+    "Daggerfol", 
+    "Roleplaying", 
+    1.5M, 
+    new DateOnly(1996, 12, 12)
+    ),
+];
+
+app.MapGet("games", () => games);
 
 app.Run();
